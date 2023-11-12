ee1cac1aac07c09a4e89de9739c9de692dd9f3f8
STAEDTLER <staedtler@STAEDTLERui-Macmini.local>
2023-11-12 17:05:17 +0900

    test

---
+++ app/src/main/java/com/example/bookmarket/BookActivity.java
@@ -0,0 +1,114 @@
+package com.example.bookmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+public class BookActivity extends AppCompatActivity {
+
+    TableLayout upperObj, lowerObj; // 1번 레이아웃 2번 레이아웃
+    ImageView leftObj, rightObj;
+
+    ImageView book11obj, book12obj, book13obj, book14obj, book21obj, book22obj,book23obj, book24obj;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+
+        upperObj = (TableLayout) findViewById(R.id.layout1);
+        lowerObj = (TableLayout) findViewById(R.id.layout2);
+        leftObj = (ImageView) findViewById(R.id.lftBtn);
+        rightObj = (ImageView) findViewById(R.id.rightBtn);
+
+        book11obj =(ImageView) findViewById(R.id.img11);
+        book12obj =(ImageView) findViewById(R.id.img12);
+        book13obj =(ImageView) findViewById(R.id.img13);
+        book14obj =(ImageView) findViewById(R.id.img14);
+
+        book21obj =(ImageView) findViewById(R.id.img21);
+        book22obj =(ImageView) findViewById(R.id.img22);
+        book23obj =(ImageView) findViewById(R.id.img23);
+        book24obj =(ImageView) findViewById(R.id.img24);
+
+
+        leftObj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {      //1번 레이아웃 보이기
+                lowerObj.setVisibility(View.INVISIBLE);
+                upperObj.setVisibility(View.VISIBLE);
+            }
+        });
+        rightObj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upperObj.setVisibility(View.INVISIBLE);
+                lowerObj.setVisibility(View.VISIBLE);
+            }
+        });
+
+        book11obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"자바 코딩의 기술", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        book12obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"머신러닝을 다루는 기술", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        book13obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"모던 리눅스 관리", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        book14obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"유니티 교과서", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        book21obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"자바 코딩의 기술", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        book22obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"머신러닝을 다루는 기술", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        book23obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"모던 리눅스 관리", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        book24obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"유니티 교과서", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/bookmarket/MainActivity.java
+++ app/src/main/java/com/example/bookmarket/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.bookmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBookClick(View view){
+        Toast.makeText(getApplicationContext(),"도서목록 클릭",Toast.LENGTH_LONG).show();
+    }
+    public void onVideoClick(View view){
+        Toast.makeText(getApplicationContext(),"동영상강좌 클릭",Toast.LENGTH_LONG).show();
+    }
+
+    public void onCustomerClick(View view){
+        Toast.makeText(getApplicationContext(),"고객센터 클릭",Toast.LENGTH_LONG).show();
+    }
+
+    public void onMyPageClick(View view){
+        Toast.makeText(getApplicationContext(),"마이페이지 클릭",Toast.LENGTH_LONG).show();
+    }
+    public void onImageClick(View view){
+        ImageView menuObj  = (ImageView) findViewById(R.id.imageView);
+        if (i %2 == 0) menuObj.setImageResource(R.drawable.cover02);
+        else menuObj.setImageResource(R.drawable.cover01);
+        i++;
     }
 }
